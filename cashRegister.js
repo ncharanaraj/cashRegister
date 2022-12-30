fd43f3f0526375158e7804e13b61a2a9290bd7ca
Charanaraj N <goudacharanraj760@gmail.com>
2022-12-30 11:18:50 +0530

    refactor: updated functionality.

--- cashRegister.js
+++ cashRegister.js
@@ -4,13 +4,13 @@ const checkButton = document.querySelector("#check-button");
 const errorMessage = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 
-const availableNotes = [2000, 500, 100, 20, 10, 5, 1]
+const availableNotes = [2000, 500, 100, 50, 20, 10, 5, 2, 1]
 
 function calculateChange(returningAmount) {
     for (let i = 0; i <= availableNotes.length; i++) {
         const numberOfNotes = Math.trunc(returningAmount / availableNotes[i]);
-        returningAmount %= availableNotes[i];
-        noOfNotes[i].innerText = numberOfNotes;
+        returningAmount = returningAmount % availableNotes[i];
+        noOfNotes[i].innerHTML = numberOfNotes;
     }
 }
 
